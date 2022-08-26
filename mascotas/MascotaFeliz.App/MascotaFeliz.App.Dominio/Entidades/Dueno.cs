9eb1773c1d087f3f24bddaf7887f017446543d55
joseph3808 <jaccfoll@gmail.com>
2022-08-25 21:57:55 -0500

    Preuba de Primer commit de Arnoldo

--- mascotas/MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/Dueno.cs
+++ mascotas/MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/Dueno.cs
@@ -5,5 +5,6 @@ namespace MascotaFeliz.App.Dominio
     public class Dueno : Persona
     {
         public string Correo { get; set; }
+        // public string email { get; set; } preuba de comit//
     }
 }
